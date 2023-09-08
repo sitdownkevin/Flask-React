4264f3b888beda4813eb88e1f27b1e0e24018af5
Ke Xu <kexu567@gmail.com>
2023-09-08 20:42:23 +0800

    run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
 start cmd /K "cd ./frontend && yarn start && exit"
-start cmd /K "cd ./backend && flask run && exit"
+start cmd /K "call .venv/Scripts/activate.bat && cd ./backend && flask run && exit"
